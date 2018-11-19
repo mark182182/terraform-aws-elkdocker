06cb64691750aef3cd01ee5b12731c7def959f33
mark182182 <versalife11@hotmail.com>
2018-11-19 17:50:40 +0100

    fix(typo): in variables.tf

--- variables.tf
+++ variables.tf
@@ -15,14 +15,14 @@ variable "aws_ami" {
 }
 variable "userdata" {
   description = "Script to install Docker and run ELK in a container (with 2048MB RAM for the container)"
-  default = "<<HEREDOC
+  default = <<HEREDOC
   #!/bin/bash
   sudo yum install docker -y
   sudo usermod -aG docker $USER
   sudo service docker restart
   sudo sysctl vm.max_map_count=262144
   sudo docker run -it -d -p 5601:5601 -p 9200:9200 -p 5044:5044 --name elkstack --ulimit nofile=65536:65536 --memory=2048m sebp/elk
-  HEREDOC"
+  HEREDOC
 }
 variable "aws_instance_type" {
   description = "AWS instance type (t2.small minimum is recommended)"
