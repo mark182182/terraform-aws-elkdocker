fcbd68804c5a28c4e50b13958bb96c44530e0f78
Vidics Márk <versalife11@hotmail.com>
2018-11-21 09:54:35 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,16 +14,15 @@ variable "aws_ami" {
   description = "AMI ID to use"
 }
 variable "userdata" {
-  description = "Script to install Docker and run ELK in a container (with 2048MB RAM for the container)"
+  description = "Script to install Docker and run ELK in a container"
   default = <<HEREDOC
   #!/bin/bash
   sudo yum install docker -y
-  sudo usermod -aG docker $USER
   sudo service docker restart
   sudo sysctl vm.max_map_count=262144
-  sudo docker run -it -d -p 5601:5601 -p 9200:9200 -p 5044:5044 --name elkstack --ulimit nofile=65536:65536 --memory=2048m sebp/elk
+  sudo docker run -it -d -p 5601:5601 -p 9200:9200 -p 5044:5044 --name elkstack --ulimit nofile=65536:65536 sebp/elk
   HEREDOC
 }
 variable "aws_instance_type" {
-  description = "AWS instance type (t2.small minimum is recommended)"
+  description = "AWS instance type (t2.medium minimum is recommended)"
 }
